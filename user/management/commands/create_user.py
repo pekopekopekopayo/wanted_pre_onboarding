f3af14d70c7430dcd1db84ca8bc6409d669d7d86
pekopekopekopayo <dlehddus1285@gmail.com>
2022-06-12 00:37:34 +0900

    [Add] user 테이블 컬럼추가

--- user/management/commands/create_user.py
+++ user/management/commands/create_user.py
@@ -1,13 +1,17 @@
 from django.core.management.base import BaseCommand
+from django.forms import ValidationError
 
 from user.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for i in range(10):
+        for i in range(5):
             try:
-                user = User(name=f"지원자{i}")
+                user = User(name=f"지원자{i}", address='경상남도 판교',phone_number=f"010-1234-567{i}")
+                user.full_clean()
                 user.save()
+            except ValidationError as e:
+                return print(e)
             except:
-                print('예상치 못한 오류가 발생되었습니다. 작업을 종료합니다')
-                return
+                return print('예상치 못한 오류가 발생되었습니다. 작업을 종료합니다')
+                
--- user/models.py
+++ user/models.py
@@ -1,5 +1,18 @@
+import re
 from django.db import models
+from django.core.validators import RegexValidator
+from django.forms import ValidationError
 
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=20, null=False, blank=False)
+    address = models.CharField(max_length=255, null=False, blank=False)
+    phone_number_regex = RegexValidator(regex = r'^01([0|1|6|7|8|9])([0-9]{4})([0-9]{4})', message='Not a valid phone number')
+    phone_number = models.CharField(validators=[phone_number_regex], max_length=11, unique=True, null=False, blank=False)
+
+
+    def full_clean(self):
+        self.phone_number = re.sub('[-]', '', str(self.phone_number))
+
+        super().full_clean()
+        

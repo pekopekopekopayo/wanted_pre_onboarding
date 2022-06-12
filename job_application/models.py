f789565d0c32bce62c670b1cce4c6b3533c06681
pekopekopekopayo <dlehddus1285@gmail.com>
2022-06-12 21:24:28 +0900

    [Modify]job_application 모델 수정 job_posting OneToOne -> OneToMany

--- job_application/models.py
+++ job_application/models.py
@@ -5,4 +5,4 @@ from job_posting.models import JobPosting
 # Create your models here.
 class JobApplication(models.Model):
     user = models.ForeignKey(User, related_name='job_applications', on_delete=models.CASCADE, null=False)
-    job_posting = models.OneToOneField(JobPosting, related_name='job_application', on_delete=models.CASCADE, null=False)
+    job_posting = models.ForeignKey(JobPosting, related_name='job_application', on_delete=models.CASCADE, null=False)

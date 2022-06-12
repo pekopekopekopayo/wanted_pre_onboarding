ef28d68bf8faeca2b50df024cb52fd75d4132375
pekopekopekopayo <dlehddus1285@gmail.com>
2022-06-12 20:02:11 +0900

    [Fix] Django 1:N관계 역참조 기능 실수로 인한 모든 migrations파일 초기화 migrate

--- job_posting/models.py
+++ job_posting/models.py
@@ -4,9 +4,8 @@ from company.models import Company
 
 class JobPosting(models.Model):
 
-    company = models.ForeignKey(Company, related_name='company', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, related_name='job_postings', on_delete=models.CASCADE)
     position = models.CharField(max_length=50, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     compensation = models.IntegerField(null=False, blank=False)
     skill = models.CharField(max_length=50, null=False, blank=False)
-# Create your models here.

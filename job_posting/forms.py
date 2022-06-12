70409a0e02317c5c298bf7238043867b9ffc464d
pekopekopekopayo <dlehddus1285@gmail.com>
2022-06-12 20:33:01 +0900

    [Ref] job_posting 유효성검사 -> Form 유효성검사로 변경

---
+++ job_posting/forms.py
@@ -0,0 +1,17 @@
+from dataclasses import fields
+from django import forms
+
+from .models import JobPosting
+
+class JobPostingForm(forms.ModelForm):
+    class Meta:
+        model = JobPosting
+        fields = '__all__'
+
+
+class JobPostingUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = JobPosting
+        exclude = ['id', 'company']
+
--- job_posting/views.py
+++ job_posting/views.py
@@ -4,55 +4,44 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from company.models import Company
+from job_posting.forms import JobPostingForm, JobPostingUpdateForm
 from .models import JobPosting
 from .serializers import JobPostingDetailSerializer, JobPostingSerializer
 from rest_framework.decorators import api_view
+
 class JobPostView(APIView):
 
     def get(self, request, format=None):
-        print('여기임?')
         queryset = JobPosting.objects.all()
         serializer  = JobPostingSerializer(queryset, many=True)
         return Response(serializer.data) 
 
     def post(self, request, format=None):
-        data = dict(request.data)
-        data['company'] = Company.objects.filter(id=data['company']).first()
-        try:
-            j_p = JobPosting(**data)
-            j_p.full_clean()
-            j_p.save()
+        j_p_form = JobPostingForm(request.data)
+        if j_p_form.is_valid():
+            j_p = j_p_form.save()
             serializer  = JobPostingSerializer(j_p)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except ValidationError as e:
-            return Response(e, status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response({'error': '예상치 못한 에러'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response(j_p_form.errors.as_data(), status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, format=None):
-        data = dict(request.data)
-        try:
-            j_p = JobPosting.objects.get(id=data.pop('id')) 
-            j_p.attr_update(data)
-            j_p.full_clean()
-            j_p.save()
-            serializer  = JobPostingSerializer(j_p)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except ValidationError as e:
-            return Response(e, status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response({'error': '예상치 못한 에러'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+        j_p = JobPosting.objects.get(id=request.data['id']) 
+        j_p_form = JobPostingUpdateForm(request.data, instance=j_p)
+        if j_p_form.is_valid():
+            j_p = j_p_form.save()
+            serializer = JobPostingSerializer(j_p)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(j_p_form.errors.as_data(), status=status.HTTP_400_BAD_REQUEST)
+        
     def delete(self, request, format=None):
         try:
             j_p = JobPosting.objects.get(id=request.GET['id']) 
             j_p.delete()
-            serializer  = JobPostingSerializer(JobPosting.objects.all(), many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_200_OK)
         except JobPosting.DoesNotExist as e:
             return Response(e, status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response({'error': '예상치 못한 에러'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @api_view(('GET',))
     def detail(request, id):
@@ -62,27 +51,22 @@ class JobPostView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except JobPosting.DoesNotExist as e:
             return Response(e, status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response({'error': '예상치 못한 에러'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @api_view(('GET',))
     def search(request):
-        try:
-            company_name = request.GET.get('company_name', '')
-            company_country = request.GET.get('company_country', '')
-            company_city = request.GET.get('company_city', '')
-            position = request.GET.get('position', '')
-            compensation = request.GET.get('compensation', '')
-            skill = request.GET.get('skill', '')
-            j_p = JobPosting.objects.filter(    
-                                                company__name__icontains=company_name,
-                                                company__country__icontains=company_country,
-                                                company__city__icontains=company_city,
-                                                position__icontains=position,
-                                                compensation__icontains=compensation,
-                                                skill__icontains=skill,
-                                            )
-            serializer  = JobPostingSerializer(j_p, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            return Response({'error': '예상치 못한 에러'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        company_name = request.GET.get('company_name', '')
+        company_country = request.GET.get('company_country', '')
+        company_city = request.GET.get('company_city', '')
+        position = request.GET.get('position', '')
+        compensation = request.GET.get('compensation', '')
+        skill = request.GET.get('skill', '')
+        j_p = JobPosting.objects.filter(    
+                                            company__name__icontains=company_name,
+                                            company__country__icontains=company_country,
+                                            company__city__icontains=company_city,
+                                            position__icontains=position,
+                                            compensation__icontains=compensation,
+                                            skill__icontains=skill,
+                                        )
+        serializer  = JobPostingSerializer(j_p, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)

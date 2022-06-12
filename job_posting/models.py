0f7613fbaa38723af3de872963fa4be0cc91c137
pekopekopekopayo <dlehddus1285@gmail.com>
2022-06-12 19:20:21 +0900

    [Modify] job_posting model 컬럼 유효성 항목 수정 및 필요없는 메소드 제거

--- job_posting/models.py
+++ job_posting/models.py
@@ -4,15 +4,9 @@ from company.models import Company
 
 class JobPosting(models.Model):
 
-    def attr_update(self, dic):
-        for k in dic.keys():
-            if hasattr(self, k):
-                setattr(self, k, dic[k])
-
     company = models.ForeignKey(Company, related_name='company', on_delete=models.CASCADE)
     position = models.CharField(max_length=50, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
-    compensation = models.IntegerField(null=False, blank=False, default=0)
-    skill = models.CharField(max_length=50, null=False, blank=True)
-
+    compensation = models.IntegerField(null=False, blank=False)
+    skill = models.CharField(max_length=50, null=False, blank=False)
 # Create your models here.
